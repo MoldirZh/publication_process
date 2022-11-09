e6f58b1dbdd672edd8d5259ef282a0510c7e9b81
Moldir Zholymbetova <moldirzholymbetova@Moldirs-MacBook-Pro.local>
2022-11-10 02:29:26 +0600

    initial registration/login

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,94 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Axios from 'axios'; 
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>CRUD APPLICATION</h1>
+  const [firstNameReg, setFirstNameReg] = useState("");
+  const [lastNameReg, setLastNameReg] = useState("");
+  const [emailReg, setEmailReg] = useState("");
+  const [passwordReg, setPasswordReg] = useState("");
 
-      <div className="form">
-        <label>Movie name:</label>
-        <input type="text" name="movieName" />
-        <label>Review:</label>
-        <input type="text" name="review" />
-        <button>Submit </button>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loginStatus, setLoginStatus] = useState(""); 
+
+  const register = () => {
+    Axios.post('http://127.0.0.1:3001/register', {
+      firstName: firstNameReg,
+      lastName: lastNameReg,
+      email: emailReg,
+      password: passwordReg 
+    }).then((response) => {
+      console.log(response);
+    });
+  }
+
+  const login = () => {
+    Axios.post('http://127.0.0.1:3001/login', {
+      email: email,
+      password: password  
+    }).then((response) => {
+      if(response.data.message) {
+        setLoginStatus(response.data.message);
+      } else {
+        setLoginStatus(response.data[0].email);
+      }
+    });
+  }
+
+  return (
+    <div className='App'>
+      <h1>REGISTRATION</h1>
+      <div className='registration'>
+        <input 
+          type='text'
+          placeholder='First name'
+          onChange={(e) => {
+            setFirstNameReg(e.target.value)
+          }}
+        />
+        <input 
+          type='text'
+          placeholder='Last name'
+          onChange={(e) => {
+            setLastNameReg(e.target.value)
+          }}
+        />
+        <input 
+          type='text'
+          placeholder='Email'
+          onChange={(e) => {
+            setEmailReg(e.target.value)
+          }}
+        />
+        <input 
+          type='text'
+          placeholder='Password'
+          onChange={(e) => {
+            setPasswordReg(e.target.value)
+          }}
+        />
+        <button onClick={register}>Register</button>
+      </div>
+      <h1>LOGIN</h1>
+      <div className='login'>
+        <input 
+          type='text'
+          placeholder='Email' 
+          onChange={(e) => {
+            setEmail(e.target.value)
+          }}
+        />
+        <input
+          type='text'
+          placeholder='Password'
+          onChange={(e) => {
+            setPassword(e.target.value)
+          }}
+        />
+        <button onClick={login}>Login</button>
+        <h1>{loginStatus}</h1>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,8 +1,52 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+const app = express();
 
-app.get("/", (req, res) => {
-    res.send("hello moldir");
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "password",
+    database: "publication",
+})
+
+app.post("/register", (req, res) => {
+
+    const email = req.body.email;
+    const password = req.body.password;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+
+    db.query(
+        "INSERT INTO users (email, password, firstName, lastName) VALUES (?,?,?,?)",
+        [email, password, firstName, lastName], 
+        (err, result) => {
+            console.log("insert", err, result);
+        }
+    );
+})
+
+app.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    db.query(
+        "SELECT * FROM users WHERE email = ? AND password = ?",
+        [email, password], 
+        (err, result) => {
+            if(err) {
+                res.send({ err: err  })
+            }
+            if(result.length > 0) {
+                res.send(result);
+            } else {
+                res.send({ message: "Wrong email/password" })
+            }
+        }
+    );
 })
 
 app.listen(3001, () => {
