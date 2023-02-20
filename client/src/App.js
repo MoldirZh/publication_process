018d37b09ef8fa794aa251aabbb0deee47ad9a95
moonlight <107063895+ayazbayainur@users.noreply.github.com>
2023-02-20 14:40:36 +0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link  } from "react-router-dom";
-import './Styles/App.css';
-import HomePage from './Pages/HomePage';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
-import Upload from './Pages/Upload';
-import ErrorPage from './Pages/ErrorPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/home/home'
+import Login from "./pages/login/login";
+import Register from "./pages/register/register";
+import Upload from "./pages/upload/upload";
 
 function App() {
   return (
-    <Router>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/upload">Upload</Link>
-      </nav>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/upload" element={<Upload />} />
-        <Route path="*" element={<ErrorPage />} />
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+        <Route path='/upload' element={<Upload/>}></Route>
       </Routes>
-      <footer>
-        Footer
-      </footer>
-    </Router>
+    </BrowserRouter>
   );
 }
 
